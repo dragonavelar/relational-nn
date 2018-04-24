68dd185bb1c0caacbbd08fc70e2f70e57f6ceedf
dragonavelar <dragonavelar@gmail.com>
2018-04-24 20:03:23 -0300

    Added a basic clone of the RN module as a class. Didn't test it to exhaustion yet.

--- model.py
+++ model.py
@@ -1,5 +1,6 @@
 import tensorflow as tf
 import numpy as np
+import random
 
 class Mlp(object):
 	def __init__(
@@ -64,4 +65,87 @@ class Mlp(object):
 	#end __call__
 #end Mlp
 
+class RN(object):
+	def __init__( self, f, reducer, g ):
+		self.__ONE_I = tf.constant(1,dtype=tf.int32)
+		self.__ZERO_I = tf.constant(0,dtype=tf.int32)
+		self.__TWO_I = tf.constant(2,dtype=tf.int32)
+		self.f = f
+		self.g = g
+		self.r = reducer
+	#end __init__
+	
+	def __call__( self, inputs, *args, **kwargs ):
+		# Save invariant values in object to avoid consuming space for being
+		# returned from the loop
+		self.n = tf.shape( inputs )[0]
+		self.n_minus_one = tf.subtract( self.n, self.__ONE_I )
+		self.inputs = inputs
+		# Guarantee that we have at least two elements on the set
+		assert_op = tf.Assert(tf.greater(self.n, 1), [self.n],name="assert_set_has_at_least_two_elements")
+		with tf.control_dependencies([assert_op]):
+			# Each pair (obj_i, obj_j), except the ones where i == j will be used
+			# So (n^2)/2 - n
+			number_of_outputs =  tf.floordiv( tf.subtract(tf.multiply( self.n, self.n ), self.n ), self.__TWO_I )
+			
+			# Define loop functions (has to be defined here to be able to call "global"
+			# variables such as self.n, self.n_minus_one and self.inputs
+			def processSetLoop(i,j,oi,output):
+				# Gather values from inputs
+				obj_i = self.inputs
+				obj_j = self.inputs
+				# Process object pair
+				output, oi = tf.cond(
+					tf.not_equal( i, j ),
+					lambda: ( output.write( oi, self.g( obj_i, obj_j ) ), tf.add( oi, self.__ONE_I ) ),
+					lambda: ( output, oi )
+				)
+				# Increment loop variables
+				i = tf.add( i, self.__ONE_I )
+				new_i, new_j = tf.cond(
+					tf.equal( i, self.n ),
+					lambda: ( tf.add( j, self.__ONE_I ), tf.add( j, self.__ONE_I ) ),
+					lambda: ( i, j )
+				)
+				return new_i, new_j, oi, output
+			#end processSetLoop
+			def processSetCond(i,j,oi,output):
+				return tf.less( j, self.n_minus_one )
+			#end processSetCond
+			
+			# Output TensorArray from the set processing
+			set_outputs = tf.TensorArray(
+				inputs.dtype,
+				size = number_of_outputs
+			)
+			# Process the set of object pairs
+			_, _, _, set_outputs = tf.while_loop(
+				processSetCond,
+				processSetLoop,
+				[self.__ONE_I,self.__ZERO_I,self.__ZERO_I,set_outputs]
+			)
+			set_outputs = set_outputs.stack()
+			reduced_output = self.r( set_outputs )
+			return self.f( reduced_output )
+		#end Control Dependencies
+		raise( Exception( "Unreachable code reached. Did you pass a set with only one element to the Relational Network module?" ) )
+	#end __call__
+#end RN
 
+if __name__ == "__main__":
+	rn = RN( Mlp([12,12,12],name="f"), lambda t: tf.reduce_sum( t, axis = 0 ), Mlp([12,12,12],name="g") )
+	rn_input = tf.placeholder( tf.float32, shape=[None,12] )
+	rn_output = rn( rn_input )
+	with tf.Session() as sess:
+		sess.run( tf.global_variables_initializer() )
+		for i in range( 2, 17 ):
+			print(
+				i,
+				sess.run(
+					rn_output,
+					feed_dict = {
+						rn_input: np.random.rand( i,12 )
+					}
+				)
+			)
+	#end Session
